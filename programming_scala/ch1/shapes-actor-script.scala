e9992d5ede72b22289b678ee0c4df73e025571e5
Luke Randall <luke.randall@gmail.com>
2010-12-03 20:34:23 +0200

    Add shapes actor

---
+++ programming_scala/ch1/shapes-actor-script.scala
@@ -0,0 +1,14 @@
+import shapes._
+
+ShapeDrawingActor.start()
+
+ShapeDrawingActor ! new Circle(new Point(0.0,0.0), 1.0)
+ShapeDrawingActor ! new Rectangle(new Point(0.0,0.0), 2, 5)
+ShapeDrawingActor ! new Triangle(new Point(0.0,0.0),
+                                 new Point(1.0,0.0),
+                                 new Point(0.0,1.0))
+
+ShapeDrawingActor ! 3.14159
+ShapeDrawingActor ! "exit"
+
+// vim: set ts=4 sw=4 et:
